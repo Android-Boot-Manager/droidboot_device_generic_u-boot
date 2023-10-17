6308c40d30ed6a140a72f7df1ebe8d9307baa0f5
Caleb Connolly <caleb.connolly@linaro.org>
2023-10-17 16:58:30 +0100

    configs: qcom: add bootmenu
    Add a boot menu with some useful default options.
    
    Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

--- include/configs/qcom.h
+++ include/configs/qcom.h
@@ -16,6 +16,12 @@
 	"stdin=serial,button-kbd\0"	\
 	"stdout=serial,vidconsole\0"	\
 	"stderr=serial,vidconsole\0" \
-	"bootcmd=bootm $prevbl_initrd_start_addr\0"
+	"preboot=bootflow scan -l\0" \
+	"bootmenu_0=Boot first available device=bootflow scan -b\0" \
+	"bootmenu_1=Enable USB mass storage=ums 0 scsi 0,1,2,3,4,5\0" \
+	"bootmenu_2=Enable fastboot mode=fastboot usb 0\0" \
+	"bootmenu_3=Reset device=reset\0" \
+	"menucmd=bootmenu\0" \
+	"bootcmd=bootflow scan -b\0" /* first entry is default */
 
 #endif
