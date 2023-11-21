c29d5032d1db3118a5f4f0c3b6a8eae341624159
Caleb Connolly <caleb.connolly@linaro.org>
2023-11-21 18:52:17 +0000

    HACK: configs: qcom: disable preboot for now
    Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

--- include/configs/qcom.h
+++ include/configs/qcom.h
@@ -16,7 +16,7 @@
 	"stdin=serial,button-kbd\0"	\
 	"stdout=serial,vidconsole\0"	\
 	"stderr=serial,vidconsole\0" \
-	"preboot=bootflow scan -l\0" \
+	"preboot=true || bootflow scan -l\0" \
 	"bootmenu_0=Boot first available device=bootflow scan -b\0" \
 	"bootmenu_1=Enable USB mass storage=ums 0 scsi 0,1,2,3,4,5\0" \
 	"bootmenu_2=Enable fastboot mode=fastboot usb 0\0" \
