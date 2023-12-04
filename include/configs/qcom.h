c01a1a865390dfd58b682cde9775a1af2f3cdcfb
Caleb Connolly <caleb.connolly@linaro.org>
2023-12-04 15:00:37 +0000

    mach-snapdragon: allocate fastboot buffer dynamically
    Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

--- include/configs/qcom.h
+++ include/configs/qcom.h
@@ -13,13 +13,15 @@
 
 /* Load addressed are calculated during board_late_init(). See arm/mach-snapdragon/board.c */
 #define CFG_EXTRA_ENV_SETTINGS \
+	"bootdelay=1\0" \
 	"stdin=serial,button-kbd\0"	\
 	"stdout=serial,vidconsole\0"	\
 	"stderr=serial,vidconsole\0" \
-	"preboot=true || bootflow scan -l\0" \
+	"preboot=bootflow scan -l\0" \
+	"fastboot=fastboot -l $fastboot_addr_r usb 0\0" \
 	"bootmenu_0=Boot first available device=bootflow scan -b\0" \
 	"bootmenu_1=Enable USB mass storage=ums 0 scsi 0,1,2,3,4,5\0" \
-	"bootmenu_2=Enable fastboot mode=fastboot usb 0\0" \
+	"bootmenu_2=Enable fastboot mode=run fastboot\0" \
 	"bootmenu_3=Reset device=reset\0" \
 	"menucmd=bootmenu\0" \
 	"bootcmd=bootflow scan -b\0" /* first entry is default */
